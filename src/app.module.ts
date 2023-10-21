7a0146165adcaaec45f444a4b3e65326b37cbdec
Md.Asif Parvez Sarker <AsifParvezsarker26@gmail.com>
2023-10-21 10:42:10 +0600

    define user controller, service and repository
    Add the content into user Controller, service, Repository and also add UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+      UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {User} from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAllUser(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOneUser(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async createUser(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async updateUser(id: number, user: User): Promise<User> {
+    await this.userRepository.update(id, user);
+    return this.userRepository.findOneBy( { id } );
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
